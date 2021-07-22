60e55e7d02ce0471de0cd55bbad469e56958fce8
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2021-07-22 21:51:50 +0700

    [Refactor , Add] change code style and add more functionable commands

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+#import token
+# from apikeys import *
+intents = discord.Intents.default()
+intents.members = True
+
+
+client = commands.Bot(command_prefix = '!', intents=intents)
+
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("The bot is now ready to use")
+    print("---------------------------")
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("สวัสดีค่าาาาาา") 
+
+@client.command()
+async def cmds(ctx):
+    await ctx.send("-----") 
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+async def on_member_join(member):
+    channel = client.get_channel('channel-id')
+    await channel.send("Helo")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel('channel-id')
+    await channel.send("Bye")
 
+# place temporary token in here just for testing 
 client.run('token here')
